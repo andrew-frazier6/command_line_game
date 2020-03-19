6f264a83b830f3ddaebf67f4751ec005333fa4e3
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-19 12:26:09 -0400

    inital game process set up

--- script.py
+++ script.py
@@ -15,6 +15,12 @@ for x in questions:
         correct_answers += 1
         total_questions -= 1
     elif user != x['back-of-card']:
-        print("my goodness you need help")
+        print("My goodness you need help")
         incorrect_answers += 1
         total_questions -= 1
+    if total_questions == 0
+    print("LET'S SEE HOW YOU DID!")
+    print(correct_answers / 25)
+    print("Wanna give it another try? y/n")
+    if user == "y":
+        flash_game()

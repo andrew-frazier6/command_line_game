2e8d0a7d359793a237d9a22330a072f9f75b3fa0
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-22 09:05:16 -0400

    question 1

--- questions.py
+++ questions.py
@@ -1,7 +1,7 @@
 questions = [
     {
-        "question": "",
-        "answer": ""
+        "question": "In 2005, Nick Swisher finished behind which teammate in the Rookie of the Year voting?",
+        "answer": "Mark Ellis/Joe Blanton/Joe Blanton/Bobby Crosby"
     }, {
         "question": "",
         "answer": ""

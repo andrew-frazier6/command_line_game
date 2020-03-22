24da80f2cbfa231586ce654753a2c7259fa69a34
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-22 11:37:23 -0400

    question 2

--- questions.py
+++ questions.py
@@ -3,8 +3,8 @@ questions = [
         "question": "In 2005, Nick Swisher finished behind which teammate in the Rookie of the Year voting?",
         "answer": "Mark Ellis/Joe Blanton/Joe Blanton/Bobby Crosby"
     }, {
-        "question": "",
-        "answer": ""
+        "question": "Tim Hudson was a member of Oakland's "Big Three" pitchers along with what other players?",
+        "answer": "Mark Mulder And Barry Zito/Mark Mulder And Barry Zito/Barry Mulder And Mark Zito/Barry Zito And Huston Street"
     }, {
         "question": "",
         "answer": ""
